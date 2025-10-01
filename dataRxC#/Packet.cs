7625ac4262aff2d5a2749c69110946bc9b2e655c
이주연 <jy122032@gmail.com>
2025-10-01 16:24:06 +0900

    데이터 수신부 구현

---
+++ dataRxC#/Packet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+public class Packet
+{
+    public UInt32 Seq { get; set; }
+    public UInt64 Timestamp { get; set; }
+    public byte[] Content { get; set; } = new byte[244];
+
+    public byte[] Serialize()
+    {
+        var buffer = new byte[256];
+        var span = buffer.AsSpan();
+
+        BitConverter.TryWriteBytes(span.Slice(0, 4), Seq);
+        if (BitConverter.IsLittleEndian) span.Slice(0, 4).Reverse();
+
+        BitConverter.TryWriteBytes(span.Slice(4, 8), Timestamp);
+        if (BitConverter.IsLittleEndian) span.Slice(4, 8).Reverse();
+
+        Content.AsSpan().CopyTo(span.Slice(12, 244));
+
+        return buffer;
+    }
+}
---
+++ dataRxC#/dataTx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class dataTx
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("데이터 수신 시작");
+        Stopwatch sw = new Stopwatch();
+
+        int duration = 60; // seconds
+        int totalPackets = duration * 500; // 500 packets per second
+        string clientIp = "127.0.0.1";
+        int clientPort = 9999;
+
+
+        // (1) UdpClient 객체 성성
+        UdpClient udpClient = new UdpClient();
+
+        sw.Start();
+
+        // (2) 데이터 송신
+        for (int i = 0; i < totalPackets; i++)
+        {
+            Packet packet = new Packet();
+            packet.Seq = (UInt32)i;
+            packet.Timestamp = (UInt64)DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+
+            byte[] datagram = packet.Serialize();
+            udpClient.Send(datagram, datagram.Length, clientIp, clientPort);
+
+            Console.WriteLine("[Send] {0}:{1} 바이트 전송", i, datagram.Length);
+
+            Thread.Sleep(2);
+        }
+
+        sw.Stop();
+
+        // (3) 자원 해제
+        udpClient.Close();
+
+        Console.WriteLine("데이터 송신 종료, {0} seconds", sw.ElapsedMilliseconds / 1000);
+    }
+}
